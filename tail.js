c425c0a0ee07d484ed130ecbfb8d61a2105b602b
Muhamed Mumin <muhamedmumin89@gmail.com>
2023-01-10 14:57:55 -0500

    Remaining elements in array

---
+++ tail.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function tail(arr){
+  return arr.slice(1)
+}
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+console.log(result)
+assertEqual(result[0], "Lighthouse");
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
